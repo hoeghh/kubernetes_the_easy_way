7dfff45928d09a27bf2cc7a84dea0a72b3a09706
Henrik René Høegh <heh@praqma.net>
2018-01-18 20:56:16 +0100

    moving output from scripts to logfiles under logs folder as of #4

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+echo "Sourcing config file..."
 source config
+
+echo "Generating hosts..."
 scripts/generate_hosts.sh > hosts
-(cd ssl; ../scripts/generate_certs.sh)
-vagrant box update
-vagrant up
+
+echo "Generating certificates..."
+(cd ssl; ../scripts/generate_certs.sh &> ../logs/cert.log)
+
+echo "Checking if vm box should be updated..."
+vagrant box update &> logs/vagrant-box-upgrade.log
+
+echo "Running Vagrant, this will take a while...
+Follow progress in logs/vagrant-provition.log"
+vagrant up &> logs/vagrant-provition.log
 
---
+++ logs/README.md
@@ -0,0 +1,5 @@
+# This folder contains logfiles for the installer.
+
+- `cert.log` contains the logs from the script that generates the certificates
+- `vagrant-box-upgrade.log` contains log from Vagrant box on upgrade progress
+- `vagrant-provition.log` contains the output from Vagrant when provitioning the VM's"
